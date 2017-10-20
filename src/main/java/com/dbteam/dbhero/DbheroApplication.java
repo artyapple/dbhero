10be71e7cda033bb0d4ec5e9c68c448900758702
Alex <aleksandr.sultanov@haw-hamburg.de>
2017-10-20 22:53:13 +0200

    initial commit

---
+++ src/main/java/com/dbteam/dbhero/DbheroApplication.java
@@ -0,0 +1,12 @@
+package com.dbteam.dbhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbheroApplication.class, args);
+	}
+}
